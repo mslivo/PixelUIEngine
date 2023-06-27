b1de5da67137f0694819ac98f70cc818abd94bee
Markus <markus.slivovksy89@gmail.com>
2023-06-27 15:39:55 +0200

    Update

--- core/src/main/java/org/mslivo/core/engine/ui_engine/misc/ViewportMode.java
+++ core/src/main/java/org/mslivo/core/engine/ui_engine/misc/ViewportMode.java
@@ -1,5 +1,12 @@
 package org.mslivo.core.engine.ui_engine.misc;
 
 public enum ViewportMode {
-    PIXEL_PERFECT, FIT, STRETCH
+    PIXEL_PERFECT("Pixel Perfect"), FIT("Fit"), STRETCH("Stretch");
+
+    public final String text;
+
+    ViewportMode(String text){
+        this.text = text;
+    }
+
 }
