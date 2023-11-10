7ae12759a2e607ec1f8c3842aad601b100f815fc
Markus <markus.slivovksy89@gmail.com>
2023-11-10 11:07:55 +0100

    ZoomOutTransition

--- core/src/main/java/org/mslivo/core/engine/tools/transitions/ZoomOutTransition.java
+++ core/src/main/java/org/mslivo/core/engine/tools/transitions/ZoomOutTransition.java
@@ -4,43 +4,39 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
-import org.mslivo.core.engine.tools.Tools;
 
-public class ZoomInTransition implements Transition {
+public class ZoomOutTransition implements Transition {
     private float zoom, zoomAcc;
     private int screenWidth;
     private int screenHeight;
+
     @Override
     public void init(int screenWidth, int screenHeight) {
         this.screenWidth = screenWidth;
         this.screenHeight = screenHeight;
-        this.zoom = 0f;
+        this.zoom = 1f;
         this.zoomAcc = 0.02f;
     }
 
     @Override
     public boolean update() {
-        this.zoom += zoomAcc;
+        this.zoom -= zoomAcc;
         zoomAcc += 0.001f;
-        return this.zoom > 1f;
+        return this.zoom < 0f;
     }
 
     @Override
     public void render(SpriteBatch batch, TextureRegion texture_from, TextureRegion texture_to) {
         batch.begin();
-
         batch.setColor(Color.WHITE);
         batch.draw(texture_to, 0, 0);
-
-        batch.setColor(1f,1f,1f,1f-zoom);
-        batch.draw(texture_from, -screenWidth*(zoom/2f),-screenHeight*(zoom/2f),screenWidth*(zoom+1), screenHeight*(zoom+1));
-        /*
-        int x = -MathUtils.round(screenWidth*zoom);
-        int y = -MathUtils.round(screenHeight*zoom);
-        batch.draw(texture_from, x, y,screenWidth*zoom*2,screenHeight*zoom*2);
-*/
-
-
+        if(zoom > 0.1f) {
+            batch.draw(texture_from,
+                    MathUtils.round(screenWidth * (1f - zoom) * 0.5f),
+                    MathUtils.round(screenHeight * (1f - zoom) * 0.5f),
+                    MathUtils.round(screenWidth * zoom),
+                    MathUtils.round(screenHeight * zoom));
+        }
         batch.end();
     }
 
