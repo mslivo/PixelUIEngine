722792d127cf40c0ff933d23415eda8b39183a23
Markus <markus.slivovksy89@gmail.com>
2024-07-10 20:40:35 +0200

    Added Thread Processing to Particle Systems Removed ThreadedListUpdater

--- desktop/src/main/java/net/mslivo/example/ParticleTest.java
+++ desktop/src/main/java/net/mslivo/example/ParticleTest.java
@@ -5,6 +5,15 @@ import com.badlogic.gdx.math.MathUtils;
 import net.mslivo.core.engine.tools.particles.immediate.PrimitiveParticle;
 import net.mslivo.core.engine.tools.particles.immediate.PrimitiveParticleSystem;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
 public class ParticleTest extends PrimitiveParticleSystem<Object> {
 
     public ParticleTest() {
@@ -24,24 +33,36 @@ public class ParticleTest extends PrimitiveParticleSystem<Object> {
         long nano = System.nanoTime();
 
         while (particleTest.particleCount() > 0) {
-            particleTest.update();
-            try {
-                Thread.sleep(1);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+            particleTest.updateParallel();
             System.out.println(particleTest.particleCount());
         }
 
-        System.out.println("finished "+((System.nanoTime()-nano)/1000));
+        System.out.println("finished " + ((System.nanoTime() - nano) / 1000));
+
 
     }
 
     @Override
     protected boolean updateParticle(PrimitiveParticle<Object> particle) {
-        particle.color_a.set(0, particle.color_a.get(0) - MathUtils.random(0.001f,0.01f));
+        particle.color_a.set(0, particle.color_a.get(0) - MathUtils.random(0.01f, 0.02f));
         if (particle.color_a.get(0) < 0) {
             return false;
+        }
+        try {
+            try (ZipFile zipFile = new ZipFile("E:\\Downloads\\04b_30(2).zip")) {
+                Enumeration<? extends ZipEntry> entries = zipFile.entries();
+                while (entries.hasMoreElements()) {
+                    ZipEntry entry = entries.nextElement();
+                    // Check if entry is a directory
+                    if (!entry.isDirectory()) {
+                        try (InputStream inputStream = zipFile.getInputStream(entry)) {
+                            // Read and process the entry contents using the inputStream
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
 
         return true;
