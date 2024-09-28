2c0063e4d8ded7efec584b9f61c66f470639f0d4
Markus <markus.slivovksy89@gmail.com>
2024-09-28 09:51:54 +0200

    ImmediateTransition added

---
+++ core/src/main/java/net/mslivo/core/engine/tools/transitions/transitions/ImmediateTransition.java
@@ -0,0 +1,43 @@
+package net.mslivo.core.engine.tools.transitions.transitions;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import net.mslivo.core.engine.tools.transitions.TRANSITION_RENDER_MODE;
+import net.mslivo.core.engine.tools.transitions.TRANSITION_SPEED;
+import net.mslivo.core.engine.tools.transitions.Transition;
+import net.mslivo.core.engine.ui_engine.rendering.SpriteRenderer;
+
+public class ImmediateTransition extends Transition {
+
+    public ImmediateTransition(){
+        super(TRANSITION_SPEED.IMMEDIATE);
+    }
+
+    @Override
+    public TRANSITION_RENDER_MODE getRenderMode() {
+        return TRANSITION_RENDER_MODE.FROM_FIRST;
+    }
+
+    @Override
+    public void init(int screenWidth, int screenHeight) {
+
+    }
+
+    @Override
+    public boolean update() {
+        return true;
+    }
+
+    @Override
+    public void renderFrom(SpriteRenderer spriteRenderer, TextureRegion texture_from) {
+    }
+
+    @Override
+    public void renderTo(SpriteRenderer spriteRenderer, TextureRegion texture_to) {
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+}
