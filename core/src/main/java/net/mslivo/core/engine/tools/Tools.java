a86a8e45268bf65f527192cd7cd9ec212f80c79b
Markus <markus.slivovksy89@gmail.com>
2024-12-10 18:40:54 +0100

    cmedia changes

--- core/src/main/java/net/mslivo/core/engine/tools/Tools.java
+++ core/src/main/java/net/mslivo/core/engine/tools/Tools.java
@@ -383,7 +383,7 @@ public class Tools {
         }
 
         public static boolean writeTextToFile(Path file, String text)  {
-            writeTextToFile(file, text, false);
+            return writeTextToFile(file, text, false);
         }
 
         public static boolean writeTextToFile(Path file, String text, boolean zipped)  {
