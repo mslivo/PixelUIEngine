942f6d1a3ba8b7c7f5b5e7bd0f3a74b5754b58a9
Markus <markus.slivovksy89@gmail.com>
2025-02-16 11:19:26 +0100

    HSL Shaders fixed

--- core/src/main/java/net/mslivo/core/engine/ui_engine/rendering/PrimitiveRenderer.java
+++ core/src/main/java/net/mslivo/core/engine/ui_engine/rendering/PrimitiveRenderer.java
@@ -57,24 +57,10 @@ public class PrimitiveRenderer {
                 return vec3(h, s, l);
             }
             
-            vec3 hsl2rgb(vec3 hsl) {
-                float h = hsl.x * 6.0;
-                float s = hsl.y;
-                float l = hsl.z;
-            
-                float c = (1.0 - abs(2.0 * l - 1.0)) * s;
-                float x = c * (1.0 - abs(mod(h, 2.0) - 1.0));
-                float m = l - 0.5 * c;
-            
-                vec3 rgb;
-                if (h < 1.0) rgb = vec3(c, x, 0.0);
-                else if (h < 2.0) rgb = vec3(x, c, 0.0);
-                else if (h < 3.0) rgb = vec3(0.0, c, x);
-                else if (h < 4.0) rgb = vec3(0.0, x, c);
-                else if (h < 5.0) rgb = vec3(x, 0.0, c);
-                else rgb = vec3(c, 0.0, x);
-            
-                return rgb + vec3(m);
+            vec3 hsl2rgb( in vec3 hsl )
+            {
+                vec3 rgb = clamp( abs(mod(hsl.x*6.0+vec3(0.0,4.0,2.0),6.0)-3.0)-1.0, 0.0, 1.0 );
+                return hsl.z + hsl.y * (rgb-0.5)*(1.0-abs(2.0*hsl.z-1.0));
             }
          
             void main() {
