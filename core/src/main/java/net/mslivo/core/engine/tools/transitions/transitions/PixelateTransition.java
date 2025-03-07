84da19b14c236dff0ee52bb4c9c14e0d92ae1fe8
Markus <markus.slivovksy89@gmail.com>
2025-03-07 14:27:32 +0100

    shaders

--- core/src/main/java/net/mslivo/core/engine/tools/transitions/transitions/PixelateTransition.java
+++ core/src/main/java/net/mslivo/core/engine/tools/transitions/transitions/PixelateTransition.java
@@ -26,7 +26,7 @@ public class PixelateTransition extends Transition {
         return TRANSITION_RENDER_MODE.FROM_FIRST;
     }
 
-    private ShaderProgram pixelationShader = new SpriteShader(Tools.File.findResource("shaders/sprite_pixelation.shader")).compile();
+    private ShaderProgram pixelationShader = new SpriteShader(Tools.File.findResource("shaders/pixelation.sprite.glsl")).compile();
 
     @Override
     public void init(SpriteRenderer spriteRenderer, int screenWidth, int screenHeight) {
