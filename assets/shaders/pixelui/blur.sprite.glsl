ece83eb53aadd3c0261bd0adb8465093fea0778c
Markus <markus.slivovksy89@gmail.com>
2025-03-13 15:24:53 +0100

    allow import of shader functions

--- assets/shaders/pixelui/blur.sprite.glsl
+++ assets/shaders/pixelui/blur.sprite.glsl
@@ -3,8 +3,8 @@ FRAGMENT:import colorModAdd blur
 
 void main(){
 
-    vec4 color = colorModAdd(texture2D(u_texture, v_texCoords),v_color);
+    vec4 color = texture2D(u_texture, v_texCoords);
     vec4 blurred = blur(v_texCoords, u_texture, u_textureSize);
 
-    vec4 fragColor = mix(color, blurred, v_tweak.x);
+    vec4 fragColor = colorModAdd(mix(color, blurred, v_tweak.x),v_color);
 }
--- assets/shaders/pixelui/functions/blur.function.glsl
+++ assets/shaders/pixelui/functions/blur.function.glsl
@@ -1,13 +1,13 @@
 vec4 blur(vec2 texCoords, sampler2D texture, vec2 textureSize) {
     vec2 texelSize = 1.0 / textureSize;
     vec4 color = texture2D(texture, texCoords) * 4.0; // Center pixel
-    color += colorMod(texture2D(texture, texCoords + vec2(texelSize.x, 0.0)),v_color);
-    color += colorMod(texture2D(texture, texCoords - vec2(texelSize.x, 0.0)),v_color);
-    color += colorMod(texture2D(texture, texCoords + vec2(0.0, texelSize.y)),v_color);
-    color += colorMod(texture2D(texture, texCoords - vec2(0.0, texelSize.y)),v_color);
-    color += colorMod(texture2D(texture, texCoords + vec2(texelSize.x, texelSize.y)),v_color);
-    color += colorMod(texture2D(texture, texCoords - vec2(texelSize.x, texelSize.y)),v_color);
-    color += colorMod(texture2D(texture, texCoords + vec2(texelSize.x, -texelSize.y)),v_color);
-    color += colorMod(texture2D(texture, texCoords - vec2(texelSize.x, -texelSize.y)),v_color);
+    color += texture2D(texture, texCoords + vec2(texelSize.x, 0.0));
+    color += texture2D(texture, texCoords - vec2(texelSize.x, 0.0));
+    color += texture2D(texture, texCoords + vec2(0.0, texelSize.y));
+    color += texture2D(texture, texCoords - vec2(0.0, texelSize.y));
+    color += texture2D(texture, texCoords + vec2(texelSize.x, texelSize.y));
+    color += texture2D(texture, texCoords - vec2(texelSize.x, texelSize.y));
+    color += texture2D(texture, texCoords + vec2(texelSize.x, -texelSize.y));
+    color += texture2D(texture, texCoords - vec2(texelSize.x, -texelSize.y));
     return color / 12.0; // Normalize
 }
